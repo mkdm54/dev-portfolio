096e20484ff7cda208fd4a61f28b8fee37760356
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-02-20 11:05:44 +0700

    setup Vite for efficient development workflow

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    root: "public", // Gunakan folder 'public' sebagai root proyek
+    build: {
+        outDir: "../dist", // Output hasil build ke folder 'dist'
+        emptyOutDir: true // Hapus isi folder 'dist' sebelum build baru
+    },
+    server: {
+        open: "index.html" // Saat `npm run dev`, langsung buka index.html
+    }
+});
